31650d792f456e522f9b746d30929c892da2b67b
Raphael Saldanha <rfsaldanha@gmail.com>
2026-02-11 08:43:11 -0300

    ws weekly 1981 2010

--- ws_weekly_1981_2010.R
+++ ws_weekly_1981_2010.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(lubridate)
 library(arrow)
 library(readr)
-library(climindi) # http://rfsaldanha.github.io/climindi/
-library(zendown) # https://rfsaldanha.github.io/zendown/
+library(climindi)
+library(zendown)
 library(cli)
 library(tibble)
 
@@ -40,17 +40,27 @@ ws_normal <- ws_data |>
   ungroup()
 
 cli_alert_info("Computing indicators...")
+
+ufs <- ws_data |>
+  mutate(uf = substr(code_muni, 0, 2)) |>
+  select(uf) |>
+  distinct(uf) |>
+  pull(uf)
+
 ws_indi <- tibble()
-for (i in 2011:2025) {
+
+for (i in ufs) {
   cli_inform("{i}")
 
   ws_indi_tmp <- ws_data |>
     # Identify year
     mutate(year = year(date)) |>
-    # Identify month
-    mutate(week = epiweek(date)) |>
     # Filter year
-    filter(year == i) |>
+    filter(year >= 2011) |>
+    # Filter UF
+    filter(substr(code_muni, 0, 2) == i) |>
+    # Identify week
+    mutate(week = epiweek(date)) |>
     # Create wave variables
     group_by(code_muni) |>
     add_wave(
@@ -98,7 +108,7 @@ for (i in 2011:2025) {
 
 
 cli_alert_info("Exporting...")
-write_parquet(x = ws_normal, sink = "ws_normal.parquet")
-write_csv2(x = ws_normal, file = "ws_normal.csv")
-write_parquet(x = ws_indi, sink = "ws_indi.parquet")
-write_csv2(x = ws_indi, file = "ws_indi.csv")
+write_parquet(x = ws_normal, sink = "ws_normal_1981_2010.parquet")
+write_csv2(x = ws_normal, file = "ws_normal_1981_2010.csv")
+write_parquet(x = ws_indi, sink = "ws_indi_1981_2010.parquet")
+write_csv2(x = ws_indi, file = "ws_indi_1981_2010.csv")
